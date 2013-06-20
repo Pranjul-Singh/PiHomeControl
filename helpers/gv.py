df020695f9d5bc1a2d2e68a2185ae7d6b590b542
Pete LePage <Pete.LePage@pobox.com>
2013-06-20 10:37:49 -0400

    gvoice sample

--- helpers/gv.py
+++ helpers/gv.py
@@ -1,11 +1,17 @@
 from subprocess import Popen, PIPE
 import json
+import logging
 
-try:
-	output = Popen(['lynx', '-source', 'https://www.google.com/voice/request/unread'], stdout=PIPE)
-	p = output.stdout.read()
-	print "completed"
-	print p
-except Exception, e:
-	print "error"
-	print str(e)
+
+def gVoiceMessages():
+    try:
+        output = Popen(['lynx', '-source', 'https://www.google.com/voice/request/unread'], stdout=PIPE)
+        resp = output.stdout.read()
+        data = json.loads(resp)
+        return data
+    except Exception, e:
+        logging.error("Unable to get Google Voice message data: " + str(e) + "\n")
+        return json.loads('{"error": "' + str(e) + '"}')
+
+
+print(json.dumps(gVoiceMessages()))

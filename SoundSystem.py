88af392ac6c3f149cd34c1ffbda2b8926d5a9ad3
Pete LePage <pete.lepage@pobox.com>
2013-09-13 19:49:40 -0400

    added additional debugging to sound system

--- SoundSystem.py
+++ SoundSystem.py
@@ -6,5 +6,6 @@ def play(file_name):
     if file_name is not None:
       cmd = "mplayer -ao alsa -really-quiet -noconsolecontrols "
       call(cmd+file_name, shell=True)
+      CloudLog.debug("SoundSystem", file_name)
   except Exception, e:
     CloudLog.error("SoundSystem.Play", "Error playing sound", e)
